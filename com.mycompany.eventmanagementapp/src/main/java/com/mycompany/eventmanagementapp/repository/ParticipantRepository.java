40281e4851b88da3e7b9b8ee5c8a5d2d4bdbc6e9
Mariam Izhar <testingapplications007@gmail.com>
2025-01-25 04:34:59 +0500

    Initial setup: Folder structure, empty class files, and GUI screens

---
+++ com.mycompany.eventmanagementapp/src/main/java/com/mycompany/eventmanagementapp/repository/ParticipantRepository.java
@@ -0,0 +1,5 @@
+package com.mycompany.eventmanagementapp.repository;
+
+public class ParticipantRepository {
+
+}
---
+++ com.mycompany.eventmanagementapp/src/main/java/com/mycompany/eventmanagementapp/view/ParticipantManagementView.java
@@ -0,0 +1,5 @@
+package com.mycompany.eventmanagementapp.view;
+
+public class ParticipantManagementView {
+
+}
