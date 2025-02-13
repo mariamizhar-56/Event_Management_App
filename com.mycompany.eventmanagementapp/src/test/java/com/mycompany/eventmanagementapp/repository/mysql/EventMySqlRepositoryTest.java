1ad481772dc7bef23fed9ee370bd816ad98417f8
Mariam Izhar <testingapplications007@gmail.com>
2025-02-14 00:35:43 +0500

    -> Comments are added in Event Repository Test Class.

--- com.mycompany.eventmanagementapp/src/test/java/com/mycompany/eventmanagementapp/repository/mysql/EventMySqlRepositoryTest.java
+++ com.mycompany.eventmanagementapp/src/test/java/com/mycompany/eventmanagementapp/repository/mysql/EventMySqlRepositoryTest.java
@@ -57,38 +57,45 @@ public class EventMySqlRepositoryTest {
 	// Test for retrieving all Events from the database when it is empty
 	@Test
 	public void testgetAllEventsWhenDatabaseIsEmpty() {
+		//Setup, Exercise & Verify
 		assertThat(eventRepository.getAllEvents()).isEmpty();
 	}
 
 	// Test for retrieving all Events when database is not empty
 	@Test
 	public void testgetAllEventsWhenDatabaseIsNotEmpty() {
+		//Setup
 		EventModel event1 = new EventModel(EVENT_NAME_1, EVENT_DATE_1, EVENT_LOCATION_1);
 		EventModel event2 = new EventModel(EVENT_NAME_2, EVENT_DATE_2, EVENT_LOCATION_2);
+		//Exercise
 		long eventId1 = addEventToTestDatabase(event1);
 		long eventId2 = addEventToTestDatabase(event2);
 
 		EventModel[] expectedEvents = new EventModel[] {
 				new EventModel(eventId1, EVENT_NAME_1, EVENT_DATE_1, EVENT_LOCATION_1),
 				new EventModel(eventId2, EVENT_NAME_2, EVENT_DATE_2, EVENT_LOCATION_2) };
-
+		//Verify
 		Assertions.assertThat(eventRepository.getAllEvents()).containsExactly(expectedEvents);
 	}
 
 	// Test for retrieving an event by Id when it is not found
 	@Test
 	public void testgetEventByIdWhenNotFound() {
+		//Setup, Exercise & Verify
 		Assertions.assertThat(eventRepository.getEventById(EVENT_ID)).isNull();
 	}
 
 	// Test for retrieving an event by Id when it is found
 	@Test
 	public void testgetEventByIdWhenFound() {
+		//Setup
 		EventModel event1 = new EventModel(EVENT_NAME_1, EVENT_DATE_1, EVENT_LOCATION_1);
 		EventModel event2 = new EventModel(EVENT_NAME_2, EVENT_DATE_2, EVENT_LOCATION_2);
+		//Exercise
 		addEventToTestDatabase(event1);
 		long eventId2 = addEventToTestDatabase(event2);
 		EventModel expectedEvent = new EventModel(eventId2, EVENT_NAME_2, EVENT_DATE_2, EVENT_LOCATION_2);
+		//Verify
 		EventModel actualEvent = eventRepository.getEventById(eventId2);
 		Assertions.assertThat(actualEvent).isEqualTo(expectedEvent);
 	}
@@ -96,15 +103,19 @@ public class EventMySqlRepositoryTest {
 	// Test for adding a new event in database
 	@Test
 	public void testAddEvent() {
+		//Setup
 		EventModel event = new EventModel(EVENT_NAME_1, EVENT_DATE_1, EVENT_LOCATION_1);
+		//Exercise
 		eventRepository.addEvent(event);
 		EventModel[] expectedEvent = new EventModel[] { event };
-		Assertions.assertThat(getAllEventsFromTestDatabase()).containsExactly(expectedEvent);
+		//Verify
+		Assertions.assertThat(eventRepository.getAllEvents()).containsExactly(expectedEvent);
 	}
 
 	// Test when adding a null event to database
 	@Test
 	public void testAddEventWhenEventIsNull() {
+		//Setup, Exercise & Verify
 		assertThatThrownBy(() -> eventRepository.addEvent(null)).isInstanceOf(HibernateException.class)
 				.hasMessageContaining("Could not add event.");
 	}
@@ -112,15 +123,19 @@ public class EventMySqlRepositoryTest {
 	// Test for deleting an event from database
 	@Test
 	public void testDeleteEvent() {
+		//Setup
 		EventModel event = new EventModel(EVENT_NAME_1, EVENT_DATE_1, EVENT_LOCATION_1);
+		//Exercise
 		addEventToTestDatabase(event);
 		eventRepository.deleteEvent(event);
-		Assertions.assertThat(getAllEventsFromTestDatabase()).isEmpty();
+		//Verify
+		Assertions.assertThat(eventRepository.getAllEvents()).isEmpty();
 	}
 
 	// Test for deleting a null event from database
 	@Test
 	public void testDeleteEventWhenEventIsNull() {
+		//Setup, Exercise & Verify
 		assertThatThrownBy(() -> eventRepository.deleteEvent(null)).isInstanceOf(HibernateException.class)
 				.hasMessageContaining("Could not delete event.");
 	}
@@ -128,31 +143,25 @@ public class EventMySqlRepositoryTest {
 	// Test for updating an existing event from database
 	@Test
 	public void testUpdateEvent() {
+		//Setup
 		EventModel event = new EventModel(EVENT_NAME_1, EVENT_DATE_1, EVENT_LOCATION_1);
+		//Exercise
 		long eventId = addEventToTestDatabase(event);
 		EventModel updatedEvent = new EventModel(eventId, EVENT_NAME_1, EVENT_DATE_2, EVENT_LOCATION_2);
 		eventRepository.updateEvent(updatedEvent);
-		Assertions.assertThat(getAllEventsFromTestDatabase()).containsExactly(new EventModel[] { updatedEvent });
+		//Verify
+		Assertions.assertThat(eventRepository.getAllEvents()).containsExactly(new EventModel[] { updatedEvent });
 	}
 
-	// Utility Functions
 	// Test for updating a null event from database
 	@Test
 	public void testUpdateEventWhenEventIsNull() {
+		//Setup, Exercise & Verify
 		assertThatThrownBy(() -> eventRepository.updateEvent(null)).isInstanceOf(HibernateException.class)
 				.hasMessageContaining("Could not update event.");
 	}
 
-	// Utility function to fetch all events from database
-	private List<EventModel> getAllEventsFromTestDatabase() {
-		Session session = sessionFactory.openSession();
-		Transaction transaction = session.beginTransaction();
-		List<EventModel> result = session.createQuery("from EventModel", EventModel.class).list();
-		transaction.commit();
-		session.close();
-		return result;
-	}
-
+	// Utility Functions
 	// Utility function to insert an event into the database.
 	private long addEventToTestDatabase(EventModel event) {
 		Session session = sessionFactory.openSession();
