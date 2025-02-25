7b09b3a1ab654bceeffcc515037edea1f7b4b1c1
Mariam Izhar <testingapplications007@gmail.com>
2025-02-25 18:36:04 +0500

    Integration Tests for Race Conditions Implemented. For Add, Update and Delete Participant Controller functionality.

---
+++ com.mycompany.eventmanagementapp/src/it/java/com/mycompany/eventmanagementapp/racecondition/ParticipantControllerRaceConditionIT.java
@@ -0,0 +1,172 @@
+package com.mycompany.eventmanagementapp.racecondition;
+
+import org.junit.Test;
+import java.util.List;
+import org.junit.Before;
+import org.mockito.Mock;
+import org.junit.ClassRule;
+import java.time.LocalDate;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+import org.hibernate.SessionFactory;
+import java.util.concurrent.TimeUnit;
+import org.hibernate.boot.MetadataSources;
+import static org.awaitility.Awaitility.await;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.containers.MySQLContainer;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.mycompany.eventmanagementapp.model.*;
+import com.mycompany.eventmanagementapp.controller.*;
+import com.mycompany.eventmanagementapp.repository.*;
+import com.mycompany.eventmanagementapp.view.screen.*;
+import com.mycompany.eventmanagementapp.repository.mysql.*;
+
+public class ParticipantControllerRaceConditionIT {
+
+	// Using MySQLContainer from Test Containers for Integration Testing
+	@SuppressWarnings("resource")
+	@ClassRule
+	public static final MySQLContainer<?> mysqlContainer = new MySQLContainer<>(DockerImageName.parse("mysql:8.0.28"))
+			.withDatabaseName("test").withUsername("test").withPassword("test");
+
+	private static SessionFactory sessionFactory;
+
+	private static StandardServiceRegistry registry;
+
+	@Mock
+	private ParticipantManagementViewScreen participantView;
+
+	private ParticipantController participantController;
+
+	private EventRepository eventRepository;
+
+	private ParticipantRepository participantRepository;
+
+	private static final long DEFAULT_EVENT_ID = -1;
+	
+	private static final String EVENT_NAME = "Music Festival";
+	
+	private static final LocalDate EVENT_DATE = LocalDate.now().plusDays(10);
+	
+	private static final String EVENT_LOCATION = "Florence";
+	
+	private static final String PARTICIPANT_NAME = "John";
+	
+	private static final String PARTICIPANT_NAME_2 = "Martin";
+	
+	private static final String PARTICIPANT_EMAIL = "John@gmail.com";
+
+	// Set up the MySQL container and Hibernate configuration
+	@BeforeClass
+	public static void setupContainer() {
+		mysqlContainer.start();
+		registry = new StandardServiceRegistryBuilder().configure("hibernate-IT.cfg.xml")
+				.applySetting("hibernate.connection.url", mysqlContainer.getJdbcUrl())
+				.applySetting("hibernate.connection.username", mysqlContainer.getUsername())
+				.applySetting("hibernate.connection.password", mysqlContainer.getPassword()).build();
+	}
+
+	// Tear down the database and stop the container after tests
+	@AfterClass
+	public static void shutdownServer() {
+		StandardServiceRegistryBuilder.destroy(registry);
+		if (sessionFactory != null) {
+			sessionFactory.close();
+		}
+		mysqlContainer.stop();
+	}
+
+	// Initialize Hibernate session factory and repository and controller before
+	// each test
+	@Before
+	public void setup() {
+		MetadataSources metadataSources = new MetadataSources(registry);
+		sessionFactory = metadataSources.buildMetadata().buildSessionFactory();
+		eventRepository = new EventMySqlRepository(sessionFactory);
+		participantRepository = new ParticipantMySqlRepository(sessionFactory);
+		participantController = new ParticipantController(participantView, participantRepository, eventRepository);
+	}
+
+	// Test Add Participant Concurrent
+	@Test
+	public void testAddParticipantConcurrent() {
+		// Setup
+		EventModel event = new EventModel(DEFAULT_EVENT_ID, EVENT_NAME, EVENT_DATE, EVENT_LOCATION);
+		eventRepository.addEvent(event);
+		ParticipantModel participant = new ParticipantModel(PARTICIPANT_NAME, PARTICIPANT_EMAIL);
+		participant.addEvent(event);
+
+		// Exercise
+		List<Thread> threads = IntStream.range(0, 10).mapToObj(i -> new Thread(() -> {
+			try {
+				participantController.addParticipant(participant, event);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		})).peek(t -> t.start()).collect(Collectors.toList());
+		await().atMost(20, TimeUnit.SECONDS).until(() -> threads.stream().noneMatch(t -> t.isAlive()));
+
+		// Verify
+		ParticipantModel savedParticipant = participantRepository.getAllParticipants().get(0);
+		assertThat(participantRepository.getAllParticipants()).containsExactly(savedParticipant);
+	}
+
+	// Test Update Participant Concurrent
+	@Test
+	public void testUpdateParticipantConcurrent() {
+		// Setup
+		EventModel event = new EventModel(DEFAULT_EVENT_ID, EVENT_NAME, EVENT_DATE, EVENT_LOCATION);
+		eventRepository.addEvent(event);
+		ParticipantModel participant = new ParticipantModel(PARTICIPANT_NAME, PARTICIPANT_EMAIL);
+		participant.addEvent(event);
+		participantRepository.addParticipant(participant);
+		eventRepository.updateEvent(event);
+		ParticipantModel savedParticipant = participantRepository.getAllParticipants().get(0);
+		savedParticipant.setParticipantName(PARTICIPANT_NAME_2);
+
+		// Exercise
+		List<Thread> threads = IntStream.range(0, 10).mapToObj(i -> new Thread(() -> {
+			try {
+				participantController.updateParticipant(savedParticipant);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		})).peek(t -> t.start()).collect(Collectors.toList());
+		await().atMost(20, TimeUnit.SECONDS).until(() -> threads.stream().noneMatch(t -> t.isAlive()));
+
+		// Verify
+		assertThat(participantRepository.getAllParticipants()).containsExactly(savedParticipant);
+	}
+
+	// Test Delete Participant Concurrent
+	@Test
+	public void testDeleteParticipantConcurrent() {
+		// Setup
+		EventModel event = new EventModel(DEFAULT_EVENT_ID, EVENT_NAME, EVENT_DATE, EVENT_LOCATION);
+		eventRepository.addEvent(event);
+		ParticipantModel participant = new ParticipantModel(PARTICIPANT_NAME, PARTICIPANT_EMAIL);
+		participant.addEvent(event);
+		participantRepository.addParticipant(participant);
+		eventRepository.updateEvent(event);
+		ParticipantModel savedParticipant = participantRepository.getAllParticipants().get(0);
+		EventModel savedEvent = eventRepository.getAllEvents().get(0);
+
+		// Exercise
+		List<Thread> threads = IntStream.range(0, 10).mapToObj(i -> new Thread(() -> {
+			try {
+				participantController.deleteParticipant(savedParticipant, savedEvent);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		})).peek(t -> t.start()).collect(Collectors.toList());
+		await().atMost(20, TimeUnit.SECONDS).until(() -> threads.stream().noneMatch(t -> t.isAlive()));
+
+		// Verify
+		assertThat(participantRepository.getAllParticipants()).isEmpty();
+	}
+}
