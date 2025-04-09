c92ccc2c16fec76bb66416d8995e34e9980ace22
mariamizhar-56 <testingapplications007@gmail.com>
2025-04-09 21:31:47 +0200

    Added wait time to sql plugin in pom.xml

--- com.mycompany.eventmanagementapp/src/it/java/com/mycompany/eventmanagementapp/dbconfigurations/DBConfigSetup.java
+++ com.mycompany.eventmanagementapp/src/it/java/com/mycompany/eventmanagementapp/dbconfigurations/DBConfigSetup.java
@@ -6,15 +6,15 @@ public class DBConfigSetup {
 
 	public static DatabaseConfiguration getDatabaseConfig() {
 		String dbServer = System.getProperty("db.server");
-		if (dbServer == null) {
+		if (dbServer.equals("mvn")) {
 			// To run on eclipse
-			databaseConfig = new TestContainerConfig();
+			databaseConfig = new MavenContainerConfig();
+
 //			databaseConfig = new MavenContainerConfig();
 
-		} else if (dbServer.equals("mvn")) {
+		} else {
 			// To run using mvn command always use mvn to start docker container without
-			// test containers
-			databaseConfig = new MavenContainerConfig();
+			databaseConfig = new TestContainerConfig();
 		}
 
 		return databaseConfig;
